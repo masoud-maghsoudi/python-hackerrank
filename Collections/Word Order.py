5f78de30b1c809f0976de83c54f23be1545730b4
shaspen <shaspen@users.noreply.github.com>
2021-12-29 09:27:33 +0330

    commit

---
+++ Collections/Word Order.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+words = OrderedDict()
+for _ in range(int(input())):
+    word = input()
+    if word in words:
+        words[word] += 1
+    else:
+        words[word] = 1
+print(len(words))
+for value in words.values():
+    print(value, end=' ')
